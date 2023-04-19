702e5c71d0397390a7d2cc299e6948dc648a44d6
easwars20 <105972474+easwars20@users.noreply.github.com>
2023-04-19 16:21:18 +0530

    first commit

---
+++ FRONTEND-INTERNSHIP-ASSIGNMENT/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'frontend-internship-assignment-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend-internship-assignment';
+}
